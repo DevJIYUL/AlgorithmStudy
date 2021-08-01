5b6fe6632429ccd8f4f7b86e8eca6e5edd7e519c
DevJIYUL <lnacles@naver.com>
2021-08-01 19:52:44 +0000

    use 3d list and figure

--- Codeup/DFSBFS/tomato_3d.py
+++ Codeup/DFSBFS/tomato_3d.py
@@ -1,41 +1,58 @@
 #https://www.acmicpc.net/problem/7569
+'''
+Use 3D list like above
+graph=[
+      [[0,0,0,0,0],
+       [0,0,0,0,0],
+       [0,0,0,0,0]]
+      [[0,0,0,0,0],
+       [0,0,0,0,0],
+       [0,0,0,0,0]]
+      ]
+
+'''
 from collections import deque
 
 M,N,H = map(int,input().split())
 graph = []
 max_ = -10
 unmatured = False
-for _ in range(N*H):
-    graph.append(list(map(int,input().split())))
+for _ in range(H):
+    temp =[]
+    for j in range(N):
+        temp.append(list(map(int,input().split())))
+    graph.append(temp)
 
-dx = [-1,0,1,0,N,-N]
-dy = [0,-1,0,1,0,0]
+dz = [1,-1,0,0,0,0]
+dx = [0,0,1,-1,0,0]
+dy = [0,0,0,0,1,-1]
 queue = deque()
-for i in range(N*H):
-    for j in range(M):
-        if graph[i][j] == 1:
-            queue.append((i,j))
+for i in range(H):
+    for j in range(N):
+        for k in range(M):
+            if graph[i][j][k] == 1:
+                queue.append((i,j,k))
 
 def bfs():
     while queue:
-        x,y = queue.popleft()
+        z,x,y = queue.popleft()
         for i in range(6):
+            nz = z + dz[i]
             nx = x + dx[i]
             ny = y + dy[i]
-            if nx < 0 or nx >= N*H or ny < 0 or ny >= M:
-                continue
-            if graph[nx][ny] == -1 or graph[nx][ny] == 1:
-                continue
-            if graph[nx][ny] == 0:
-                queue.append((nx,ny))
-                graph[nx][ny] = graph[x][y] + 1
+            if 0 <= nz < H and 0 <= ny < M and 0 <= nx < N and graph[nz][nx][ny] == 0:
+                queue.append((nz,nx,ny))
+                graph[nz][nx][ny] = graph[z][x][y] + 1
 
 bfs()
+
 for i in graph:
     for j in i:
-        max_ = max(max_, j)
-        if j == 0:
-            unmatured = True
+        for k in j:
+            if k == 0:
+                unmatured = True
+            max_ = max(max_, k)
+
 if unmatured:
     print(-1)
 elif max_ == 1:
